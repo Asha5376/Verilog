ed03bf90fffec19b661fbf45437dd3bc1a53e527
Asha k <ashak90052@gmail.com>
2026-04-16 22:16:51 +0530

    Update 1.mux_4to1.v

--- mux_4to1/1.mux_4to1.v
+++ mux_4to1/1.mux_4to1.v
@@ -1,16 +1,17 @@
-module mux_4to1(a, b, c, d, sel, y);
-
-input a, b, c, d;
-input [1:0] sel;
-output reg y;
+module mux4to1 (
+    input a, b, c, d,
+    input [1:0] sel,
+    output reg y
+);
 
 always @(*) begin
-  case (sel)
-    2'b00: y = a;
-    2'b01: y = b;
-    2'b10: y = c;
-    2'b11: y = d;
-  endcase
+    case (sel)
+        2'b00: y = a;
+        2'b01: y = b;
+        2'b10: y = c;
+        2'b11: y = d;
+        default: y = 0;
+    endcase
 end
 
 endmodule
